66eac5fc5858be40f77c564d3611aac165a9e8da
MA Ahi <bijoyrashidul21@gmail.com>
2025-05-03 16:56:24 +0600

    new add file

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 const PORT = 3001;
 
 app.use("/images", express.static(path.join(__dirname, "public/images")));
+app.use(
+  "/turntable-parts",
+  express.static(path.join(__dirname, "public/turntable-parts"))
+);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
